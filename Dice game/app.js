b17ef7af8ca2b784d4e5c8d3e427d3838ead5741
Tarun Bisht <77788209+tarunbisht-24@users.noreply.github.com>
2021-10-18 22:56:00 +0530

    Added javascript for the dice game

---
+++ Dice game/app.js
@@ -0,0 +1,25 @@
+//for Dice 1
+var randomNumber1 = Math.floor(Math.random()*6) + 1; //generate random number between 1 to 6
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+//for Dice 2
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//Decide winners
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="🏁DRAW🏁";
+}
